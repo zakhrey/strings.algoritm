7558ff5b9596f4ef064e118331bae167533fa699
zakhrey <zakhrey@gmail.com>
2025-09-12 21:47:55 +0300

    initial commit. added service to count duplicate characters in string

---
+++ src/main/java/ru/zakhrey/strings/algoritm/servise/CountDuplicateCharsService.java
@@ -0,0 +1,12 @@
+package ru.zakhrey.strings.algoritm.servise;
+
+public interface CountDuplicateCharsService {
+
+    /**
+     * Подсчет повторяющихся символов
+     * @param s трока
+     * @return количество повторяющихся символов
+     */
+    int countDuplicateChars(final String s);
+
+}
